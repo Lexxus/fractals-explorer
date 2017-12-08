1547b969af3bf28459893cde87d8d8929271cb36
Oleksii Teterin <altmoc@gmail.com>
2017-12-08 18:27:48 +0200

    Some code refactoring

--- js/fractal.js
+++ js/fractal.js
@@ -1,79 +1,86 @@
-function fractal(canvas, cfg, callback){
-    var ctx = canvas.getContext(('2d')),
-        w = canvas.width,
-        h = canvas.height,
-		x = cfg.x, y = cfg.y, cY = 0,
-		width = cfg.width, height = cfg.height,
-        iLimit = cfg.n, iLimit12 = iLimit >> 1, iLimit1 = ~~(iLimit12 / 5),
-        stepX = width / w, stepY = stepX;
+function fractal(canvas, cfg, callback) {
+    const ctx = canvas.getContext('2d');
+    const w = canvas.width;
+    const h = canvas.height;
+    const width = cfg.width;
+    const height = cfg.height;
+    const iLimit = cfg.n;
+    const iLimit12 = iLimit >> 1;
+    const iLimit1 = ~~(iLimit12 / 5);
+    const stepX = width / w;
+    const stepY = stepX;
+    const imgData = ctx.getImageData(0,0, w,1);
+    const iLen = imgData.data.length;
+    const kc1 = 0xFF / iLimit1;
+    const kc2 = 0xFF / iLimit12;
+    const kc3 = 128 / iLimit1;
+    let x = cfg.x;
+    let y = cfg.y;
+    let cY = 0;
 
-    if(height){
+    if (height) {
         stepY = height / h;
     }
 
-    var imgData = ctx.getImageData(0,0, w,1),
-		iLen = imgData.data.length,
-        kc1 = 0xFF / iLimit1,
-		kc2 = 0xFF / iLimit12,
-		kc3 = 128 / iLimit1;
-
     step();
 
-    function step(){
-        if(cY >= h){
+    function step() {
+        let n = 0;
+        let i = 0;
+        let r, g, b;
+
+        if (cY >= h) {
             doF = false;
-			if(typeof callback === 'function'){
-				callback();
-			}
+            if (typeof callback === 'function') {
+                callback();
+            }
             return;
         }
 
-        if(!doF){
+        if (!doF) {
             setTimeout(arguments.callee, 1000);
             return;
         }
 
-        var n = 0, i = 0,
-			r, g, b;
-        while(i < iLen){
-			n = cfg.fn(x, y, iLimit);
+        while(i < iLen) {
+            n = cfg.fn(x, y, iLimit);
 
-            if(n < iLimit){
-				var iLimit2 = iLimit1 << 1;
+            if (n < iLimit) {
+                var iLimit2 = iLimit1 << 1;
 
-				if(n < iLimit1){
-					r = 128 - kc3 * n;
-					g = 128 - kc3 * n;
-					b = 255;
-				}else if(n < iLimit2){
-					n -= iLimit1;
-					r = 0;
-					g = kc1 * n;
-					b = 255;
-				}else if(n < iLimit2+iLimit1){
-					n -= iLimit2;
-					r = 0;
-					g = 255;
-					b = 255 - kc1 * n;
-				}else if(n < iLimit2+iLimit2){
-					n -= iLimit2+iLimit1;
-					r = kc1 * n;
-					g = 255;
-					b = 0;
-				}else if(n < iLimit12){
-					n -= iLimit2+iLimit2;
-					r = 255;
-					g = 255 - kc1 * n;
-					b = 0;
-				}else{
-					n -= iLimit12;
-					r = 255;
-					g = 0;
-					b = kc2 * n;
-				}
-			}else{
-				r = g = b = 0;
-			}
+                if (n < iLimit1) {
+                    r = 128 - kc3 * n;
+                    g = 128 - kc3 * n;
+                    b = 255;
+                } else if (n < iLimit2) {
+                    n -= iLimit1;
+                    r = 0;
+                    g = kc1 * n;
+                    b = 255;
+                } else if (n < iLimit2 + iLimit1) {
+                    n -= iLimit2;
+                    r = 0;
+                    g = 255;
+                    b = 255 - kc1 * n;
+                } else if (n < iLimit2 + iLimit2) {
+                    n -= iLimit2 + iLimit1;
+                    r = kc1 * n;
+                    g = 255;
+                    b = 0;
+                } else if (n < iLimit12) {
+                    n -= iLimit2 + iLimit2;
+                    r = 255;
+                    g = 255 - kc1 * n;
+                    b = 0;
+                } else {
+                    n -= iLimit12;
+                    r = 255;
+                    g = 0;
+                    b = kc2 * n;
+                }
+            } else {
+                r = g = b = 0;
+            }
 
             //r = (color & 0xFF0000) >> 16;
             //g = (color & 0xFF00) >> 8;
@@ -89,7 +96,7 @@ function fractal(canvas, cfg, callback){
         x = cfg.x;
         y -= stepY;
 
-		ctx.putImageData(imgData, 0, cY++);
+        ctx.putImageData(imgData, 0, cY++);
         setTimeout(arguments.callee, 0);
     }
 }
---
+++ js/fractals.js
@@ -0,0 +1,160 @@
+window.fractals = {
+    Mandelbrot2: {
+        code: "var zX = x, zY = y, z = .0, n = 0,\n\
+zX2 = x*x, zY2 = y*y;\n\
+while((zX2 + zY2) < 16 && n < limit){\n\
+z = zX2 - zY2 + x;\n\
+zY = ((zX * zY)*2) + y;\n\
+zX = z;\n\
+zX2 = zX*zX;\n\
+zY2 = zY*zY;\n\
+++n;\n\
+}\n\
+return n;",
+        cfg: {
+            x: -2,
+            y: 1.1,
+            w: 2.5
+        },
+        formula: 'Z<sub>n+1</sub> = Z<sub>n</sub><sup>2</sup> + C; <small>Z<sub>0</sub> = 0, C = x + iy</small>'
+    },
+    Mandelbrot3: {
+        code: "var zX = x, zY = y, z = .0, n = 0,\n\
+zX2 = x*x, zY2 = y*y;\n\
+while((zX2 + zY2) < 16 && n < limit){\n\
+z = (zX2 - 3*zY2)*zX + x;\n\
+zY = (3*zX2 - zY2)*zY + y;\n\
+zX = z;\n\
+zX2 = zX*zX;\n\
+zY2 = zY*zY;\n\
+++n;\n\
+}\n\
+return n;",
+        cfg: {
+            x: -1.5,
+            y: 1.32,
+            w: 3
+        },
+        formula: 'Z<sub>n+1</sub> = Z<sub>n</sub><sup>3</sup> + C; <small>Z<sub>0</sub> = 0, C = x + iy</small>'
+    },
+    Mandelbrot4: {
+        code: "var zX = x, zY = y, z = .0, n = 0,\n\
+zX2 = x*x, zY2 = y*y;\n\
+while((zX2 + zY2) < 16 && n < limit){\n\
+z = zX2*(zX2 - 6*zY2) + zY2*zY2 + x;\n\
+zY = 4*zX*zY*(zX2 - zY2) + y;\n\
+zX = z;\n\
+zX2 = zX*zX;\n\
+zY2 = zY*zY;\n\
+++n;\n\
+}\n\
+return n;",
+        cfg: {
+            x: -1.5,
+            y: 1.13,
+            w: 2.6
+        },
+        formula: 'Z<sub>n+1</sub> = Z<sub>n</sub><sup>4</sup> + C; <small>Z<sub>0</sub> = 0, C = x + iy</small>'
+    },
+    Mandelbrot5: {
+        code: "var zX = x, zY = y, z=.0, n=0,\n\
+zX2 = x*x, zY2 = y*y, zX4=.0, zY4=.0, zXY10=.0;\n\
+while((zX2 + zY2) < 16 && n < limit){\n\
+zX4 = zX2*zX2;\n\
+zY4 = zY2*zY2;\n\
+zXY10 = 10*zX2 *zY2;\n\
+z = zX*(zX4 + 5 *zY4 - zXY10) + x;\n\
+zY = zY*(zY4 + 5 *zX4 - zXY10) + y;\n\
+zX = z;\n\
+zX2 = zX*zX;\n\
+zY2 = zY*zY;\n\
+++n;\n\
+}\n\
+return n;",
+        cfg: {
+            x: -1.5,
+            y: 1.13,
+            w: 2.6
+        },
+        formula: 'Z<sub>n+1</sub> = Z<sub>n</sub><sup>5</sup> + C; <small>Z<sub>0</sub> = 0, C = x + iy</small>'
+    },
+    Julia2: {
+        code: "var zX = x, zY = y, z = .0, n = 0,\n\
+zX2 = x*x, zY2 = y*y;\n\
+while((zX2 + zY2) < 16 && n < limit){\n\
+z = zX2 - zY2 + .24;\n\
+zY = ((zX * zY)*2) - .56;\n\
+zX = z;\n\
+zX2 = zX*zX;\n\
+zY2 = zY*zY;\n\
+++n;\n\
+}\n\
+return n;",
+        cfg: {
+            x: -1.3,
+            y: 1.15,
+            w: 2.64
+        },
+        formula: 'Z<sub>n+1</sub> = Z<sub>n</sub><sup>2</sup> + C; <small>Z<sub>0</sub> = x + iy, C = 0.24 - i0.56</small>'
+    },
+    Julia3: {
+        code: "var zX = x, zY = y, z = .0, n = 0,\n\
+zX2 = x*x, zY2 = y*y;\n\
+while((zX2 + zY2) < 16 && n < limit){\n\
+z = (zX2 - 3*zY2)*zX + .51111111;\n\
+zY = (3*zX2 - zY2)*zY - .10000000;\n\
+zX = z;\n\
+zX2 = zX*zX;\n\
+zY2 = zY*zY;\n\
+++n;\n\
+}\n\
+return n;",
+        cfg: {
+            x: -1.3,
+            y: 1.05,
+            w: 2.45
+        },
+        formula: 'Z<sub>n+1</sub> = Z<sub>n</sub><sup>3</sup> + C; <small>Z<sub>0</sub> = x + iy, C = 0.51 - i0.1</small>'
+    },
+    Julia4: {
+        code: "var zX = x, zY = y, z = .0, n = 0,\n\
+zX2 = x*x, zY2 = y*y;\n\
+while((zX2 + zY2) < 16 && n < limit){\n\
+z = zX2*(zX2 - 6*zY2) + zY2*zY2 + .65;\n\
+zY = 4*zX*zY*(zX2 - zY2) - .2;\n\
+zX = z;\n\
+zX2 = zX*zX;\n\
+zY2 = zY*zY;\n\
+++n;\n\
+}\n\
+return n;",
+        cfg: {
+            x: -1.3,
+            y: 1.05,
+            w: 2.45
+        },
+        formula: 'Z<sub>n+1</sub> = Z<sub>n</sub><sup>4</sup> + C; <small>Z<sub>0</sub> = x + iy, C = 0.65 - i0.2</small>'
+    },
+    Julia5: {
+        code: "var zX = x, zY = y, z=.0, n=0,\n\
+zX2 = x*x, zY2 = y*y, zX4=.0, zY4=.0, zXY10=.0;\n\
+while((zX2 + zY2) < 16 && n < limit){\n\
+zX4 = zX2*zX2;\n\
+zY4 = zY2*zY2;\n\
+zXY10 = 10*zX2 *zY2;\n\
+z = zX*(zX4 + 5 *zY4 - zXY10) + .7;\n\
+zY = zY*(zY4 + 5 *zX4 - zXY10) - .15;\n\
+zX = z;\n\
+zX2 = zX*zX;\n\
+zY2 = zY*zY;\n\
+++n;\n\
+}\n\
+return n;",
+        cfg: {
+            x: -1.3,
+            y: 1.1,
+            w: 2.5
+        },
+        formula: 'Z<sub>n+1</sub> = Z<sub>n</sub><sup>5</sup> + C; <small>Z<sub>0</sub> = x + iy, C = 0.7 - i0.15</small>'
+    }
+};
